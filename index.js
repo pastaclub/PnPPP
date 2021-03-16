0cebae04faf0415b8dbf7e1e9f9014d78c593aef
Dennis B <dennis@bemmann.de>
2021-03-16 21:05:46 +0700

    Refactored index.js

--- index.js
+++ index.js
@@ -5,9 +5,33 @@ const csv = require('csv');
 var config;
 var cache = {};
 
-const quit = (msg) => {
+function quit(msg) {
   console.log('ERROR: ', msg);
   process.exit(1);
+}
+
+function selectColumns(data, colConfig) {  // restrict to relevant columns and rename them
+  for (var i=0; i<data.length; i++) {
+    var part = {};
+    Object.keys(colConfig).forEach((oldColName) => {
+      var newColName = colConfig[oldColName];
+      part[newColName] = data[i][oldColName];
+    });
+    data[i] = part;        
+  }
+}
+
+function writeCSV(data, fileName) {
+  console.log('Writing '+fileName);
+  csv.stringify(data, {header: true, quoted: true, record_delimiter:"\r\n"}, (err, output) => {
+    if (err) quit('Unable to transform data back into CSV');
+    fs.writeFileSync(fileName, output);
+  })
+}
+
+function processPnp(data, projectPath) {
+  selectColumns(data, config.pnp.columns); // restrict to relevant columns and rename them
+  writeCSV(data, projectPath + config.pnp.outputFileName); // generate new pick and place file
 }
 
 // read config
@@ -25,38 +49,23 @@ watch(config.baseDir, {recursive: true}, function(ev, fileName) {
   if (ev != 'update') return; // we only care about update events
   console.log('%s changed.', fileName);
 
-  // pick and place file
+  const matchBom = fileName.match(bomRegEx);
+  const matchPnp = fileName.match(pnpRegEx);  
   if (matchPnp) {
     console.log('Processing '+fileName);
-    var projectPath = matchPnp[1];
 
     // read file and split into lines
+    console.log('Processing '+fileName);
     var lines = fs.readFileSync(fileName).toString().split(/\r?\n/);
 
     // remove empty lines
     var text = '';
     for (var i=0; i<lines.length; i++) if (lines[i].length > 5) text += lines[i] + "\r\n";
- 
-    csv.parse(text, {columns: true}, (err, pnp) => {
-      if (err) quit('Unable to parse pick and place file');
- 
-      // restrict to relevant columns and rename them
-      for (var i=0; i<pnp.length; i++) {
-        var part = {};
-        Object.keys(config.pnp.columns).forEach((oldColName) => {
-          var newColName = config.pnp.columns[oldColName];
-          part[newColName] = pnp[i][oldColName];
-        });
-        pnp[i] = part;        
-      }
 
-      // generate new pick and place file
-      const pnpOutputFileName = projectPath + config.pnp.outputFileName;
-      console.log('Writing '+fileName);
-      csv.stringify(pnp, {header: true, quoted: true, record_delimiter:"\r\n"}, (err, output) => {
-        if (err) quit('Unable to transform pick and place data back into CSV');
-        fs.writeFileSync(pnpOutputFileName, output);
-      })
-  });
+    // parse CSV
+    csv.parse(text, {columns: true}, (err, data) => {
+      if (err) quit('Unable to parse CSV');
+      if (matchPnp) processPnp(data, matchPnp[1]);
+    }); 
   }
 });
